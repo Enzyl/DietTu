b33fcf434d86c995dd7efc3557f5bcc77e09905d
Mikolaj <rockmetal12.rm@gmail.com>
2024-09-17 07:18:28 +0200

    Test commit

--- src/main/java/org/example/domain/DailyDiet.java
+++ src/main/java/org/example/domain/DailyDiet.java
@@ -1,4 +1,15 @@
 package org.example.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class DailyDiet {
+    private Long dailyDietId;
+    private Meal meal;
 }
